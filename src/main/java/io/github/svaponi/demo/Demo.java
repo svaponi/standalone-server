dca2272865aae930d7da388362f589b00abbab4c
Sam <samuel.vaponi.developer@gmail.com>
2018-05-19 13:57:01 +0200

    first commit

---
+++ src/main/java/io/github/svaponi/demo/Demo.java
@@ -0,0 +1,4 @@
+package io.github.svaponi.demo;
+
+public class Demo {
+}
---
+++ src/test/java/io/github/svaponi/demo/DemoTest.java
@@ -0,0 +1,17 @@
+package io.github.svaponi.demo;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DemoTest {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("\n\trunning unit-tests\n");
+    }
+
+    @Test
+    public void test() throws Exception {
+        // do nothing ...
+    }
+}
